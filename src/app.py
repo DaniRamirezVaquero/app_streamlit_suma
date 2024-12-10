2a5238dd687a3ee6fba4ce9287d19511fe4294a6
Dani Vakero <dramvaq@gmail.com>
2024-12-10 11:04:20 +0100

    removed styles

--- src/app.py
+++ src/app.py
@@ -5,17 +5,7 @@ st.title('Aplicación suma streamlit')
 num1 = st.number_input('Ingrese el primer número', value=0)
 num2 = st.number_input('Ingrese el segundo número', value=0)
 
-# Button to perform the addition with custom hover color
-button_style = """
-  <style>
-  .stButton button:hover {
-    background-color: #4CAF50;
-    color: white;
-  }
-  </style>
-  """
-st.markdown(button_style, unsafe_allow_html=True)
-
+# Button to perform the addition
 if st.button('Sumar'):
   resultado = num1 + num2
   st.success(f'{num1} + {num2} = {resultado}')
